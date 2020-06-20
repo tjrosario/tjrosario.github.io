1b25efc2c14945490ff464184bd79a4947214407
Tommy Rosario <tom.jason.rosario@gmail.com>
2020-06-20 12:18:47 -0400

    updated bio

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -100553,7 +100553,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var ABOUT = ["And welcome! I'm a senior frontend specialist and web engineer with several years of experience developing small to large scale web sites and applications for some of the world's most recognized brands including including iHeartRadio, Topps, Estee Lauder, Lowes, Virgin Mobile, Citi, and many more.", "I have a ongoing passion for the evolving web and enjoy helping companies bring their visions to life through technology."];
+var ABOUT = ["Hello!  I'm a frontend developer based out of the Jersey Shore.  I love working on fun, innovative websites.  I've been involved in technology for almost two decades now.  My journey has given me the pleasure to work with some of the world's most recognized brands including iHeartRadio, Topps, Estee Lauder, Lowes, Virgin Mobile, Citi and many others.  I've also been lucky enough to work along side with some highly talented technologists as my peers, which I have looked to as mentors throughout my career.", "I'm a bit of a perfectionist and take a lot of pride in all of my work.  I try to leave every piece of code a bit more modular, performance and readable than how I found it.  The web is constantly evolving and I do my part in staying up to date with latest trends and solutions.", "Beyond the keyboard I have a huge passion for music, fitness and the beach!  Family and a healthy work-life balance is key to my happiness.", "I'm always looking for new opportunities and believe, with my extensive experience and passion for technology, I can be an asset to your team.", "Let's chat about how I can be a good fit for your company!"];
 var _default = ABOUT;
 exports.default = _default;
 },{}],"src/common/data/skillset.js":[function(require,module,exports) {
@@ -110407,7 +110407,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34921" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33383" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
