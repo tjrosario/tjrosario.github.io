cd106d9378661e7fc6851962905733f49148111d
Tommy Rosario <tom.jason.rosario@gmail.com>
2020-07-31 18:46:32 -0400

    beefed up resume with bullet points

--- src.a2b27638.js
+++ src.a2b27638.js
@@ -100555,13 +100555,13 @@ function (_React$Component) {
         gutterBottom: true
       }, _react.default.createElement("i", {
         className: classes.timestamp + " far fa-calendar-alt"
-      }), " ", data.date), _lodash.default.map(data.description, function (para, idx) {
-        return _react.default.createElement(_Typography.default, {
+      }), " ", data.date), _react.default.createElement("ul", null, _lodash.default.map(data.description, function (para, idx) {
+        return _react.default.createElement("li", null, _react.default.createElement(_Typography.default, {
           variant: "body1",
           gutterBottom: true,
           key: idx
-        }, para);
-      }));
+        }, para));
+      })));
     }
   }]);
 
@@ -100603,57 +100603,47 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.default = void 0;
-var EXPERIENCE = [
-/*   {
-    title: 'Frontend Developer',
-    company: 'Plixer',
-    date: 'September 2019 – Present',
-    description: [
-      "Work within the product development team re-architecting their network traffic analysis product from a legacy tech-stack to modern Angular 8+ framework."
-    ],
-    technology: "Angular 8+, Typescript, Sass"
-  }, */
-{
+var EXPERIENCE = [{
+  title: 'Frontend Developer',
+  company: 'Plixer',
+  date: 'September 2019 – Present',
+  description: ['Assisted in re-architecting network traffic analysis system from a legacy tech-stack to modern Angular 8+ framework', 'Lead development on the application’s reporting engine which gives customer’s a clear visualization of all network conversation and device usage', 'Components and services strongly typed using Typescript', 'Wrote unit tests using Jasmine and Karma', 'Implemented Bootstrap for the UI library'],
+  technology: "Angular 8+, Typescript, Sass"
+}, {
   title: 'Frontend Engineer',
   company: 'CityBldr',
   date: 'May 2017 – May 2020',
-  description: ["Leading user interface development for product platform and consumer sites. Technology leverages Artificial Intelligence, machine learning and data science to determine the best value and uses for land for both sellers and buyers."],
+  description: ['Lead front end developer for both the consumer facing and internal RETech application that determines the best value and uses for land for both sellers and buyers.', 'Built property claim flow which allows sellers to register and look up their property to determine its worth when sold alone and along with neighbors', 'Assisted in building home-grown CRM system that partners could use to manage leads and potential targets', 'Helped build a target dispatching platform which automates the process of building a set of target properties for any particular lead run', 'Facilitated ongoing upgrades to the front end stack (React)', 'State management using Redux', 'Implemented UI components with Material UI', 'Lead efforts in the consumer-facing site redesign'],
   technology: "React, Redux, LESS"
 }, {
   title: 'Sr. UI Developer',
   company: 'AffinityX',
   date: 'Jan 2015 – May 2020',
-  description: ["Senior developer on building company’s proprietary creative content delivery platform tailored for SMB’s requiring a white label digital advertising and marketing solution.  The platform simplifies the entire process from fulfillment, order tracking to production.  Stand out features include a fully integrated proofing system and complete administrative console."],
+  description: ['Created new functionality and features for creative content delivery platforms using KnockoutJS, jQuery and PHP', 'Helped in building custom application routing system', 'Developed re-usable components using KnockoutJS', 'Built a custom proofing interface that allows customers to upload creative content, manage revision history and mark for approval', 'Assisted in building administrative console'],
   technology: "React, Redux, LESS"
 }, {
   title: 'Frontend Engineer',
   company: 'ThreadLab Inc',
   date: 'Mar 2014 – Jan 2020',
-  description: ["Assisted in building a solution for a men's shopping service platform for males who hate to shop. Focused on menswear basics and essentials from well known brands. The service uses technology, rather than the typical stylist, to ensure customers receive perfectly fitting garments."],
+  description: ['Lead front end development using Angular 5+, SASS', 'Built a proxy using NodeJS for restful API consumption', 'Developed build pipeline with Jenkins', 'Created responsive layout using SASS/Bourbon', 'Implemented recurring subscription system using Recurly', 'Integrated Stripe and Paypal payment funnels', 'Manage email marketing templates via Mailchimp and Mandrill'],
   technology: "Angular 5+, NodeJS, ES6 (2015), SASS, Bourbon/Neat, Grunt, Stripe, Paypal, Groovy on Grails, MongoDB"
-}, {
-  title: 'Frontend Developer',
-  company: 'Decogram Corporation',
-  date: 'Jan 2010 – Jan 2016',
-  description: ['Lead Frontend development on a variety of client engagements.'],
-  technology: "jQuery, SCSS, Ruby on Rails"
 }, {
   title: 'Software Engineer',
   company: 'Do Something',
   date: 'May 2014 – Jan 2015',
-  description: ['Contributed to open-source Drupal platform, interface framework and pattern libraries.'],
+  description: ['Lead development on various social awareness campaigns', 'Creating new features, functionality to application modules using jQuery and RequireJS', 'Ongoing maintenance within Drupal framework template system', 'Contributed to open-sourced interface framework and pattern libraries'],
   technology: "Drupal, RequireJS, jQuery, SASS, Bourbon / Neat, Stripe, Wordress"
 }, {
   title: 'Senior Software Engineer',
   company: 'CPXi',
   date: 'Oct 2013 – May 2014',
-  description: ['Senior application developer for Consumer Apps and Real-time divisions. Building various products such as browser plugins and real-time data-driven applications.'],
+  description: ['Worked on new functionality and features for company\’s internal metrics dashboard', 'Developed browser plugins that were available in the Chrome and Firefox web stores', 'Lead UI development on a real-time news feed application / scraper built on HandlebarsJS, NodeJS, and Sails MVC', 'Proprietary CSS framework using LESS'],
   technology: "Handlebars JS, Laravel, Symfony, LESS, NodeJS, Sails MVC"
 }, {
   title: 'Frontend Tech Lead',
   company: 'Alexander Interactive',
   date: 'Jul 2006 – Oct 2013',
-  description: ['Built, managed and lead a team of seven Frontend developers. In charge of providing Frontend development estimates on all projects. Developed and re-wrote several re-usable JavaScript widgets from Prototype JS to jQuery. Introduced HTML5 and CSS3 to Magento applications. Defined and authored the company’s best practices and coding standards document for Frontend development. Worked closely with IA and Creative departments on defining new and effective user experiences. Worked closely with backend development team on Frontend integration points. Provided technical direction for clients on Frontend hand-off projects. Utilized text replacement technologies such as Typekit, Cufón, sIFR. Skinned and integrated custom Magento themes. Object-oriented JavaScript development. Responsive CSS development. Converted Photoshop documents into HTML and CSS templates. Wrote custom CSS reset framework.'],
+  description: ['Built, managed and lead a team of seven front-end developers', 'In charge of providing front-end development estimates on all projects', 'Developed and re-wrote several re-usable JavaScript widgets from Prototype JS to jQuery', 'Introduced HTML5 and CSS3 to Magento applications', 'Defined and authored the company\'s best practices and coding standards document for front-end development', 'Worked closely with IA and Creative departments on defining new and effective user experiences', 'Worked closely with backend development team on front-end integration points', 'Provided technical direction for clients on front-end handoff projects', 'Utilized text replacement technologies such as Typekit, Cufón, sIFR', 'Skinned and integrated custom Magento themes', 'Object-oriented JavaScript development', 'Responsive CSS', 'Parallax UI Development', 'Converted Photoshop documents into HTML and CSS templates', 'Wrote custom CSS reset framework'],
   technology: "jQuery, PrototypeJS, Vanilla JS, Magento, Django"
 }];
 var _default = EXPERIENCE;
@@ -110486,7 +110476,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38145" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46719" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
